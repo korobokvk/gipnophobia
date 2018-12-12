abed9548ee521f0f1318a470f0ed273bd184d9b9
korobokvk <korobokvk@gmail.com>
2018-12-12 17:14:50 +0200

    added view

---
+++ server/public/assets/js/text-content.js
@@ -0,0 +1,19 @@
+//Default about quest text
+const TEXT_ABOUT_QUEST = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ut iusto adipisci id soluta eligendi, magni similique molestiae. Illo culpa mollitia dolore, neque cumque aliquid tenetur. Voluptates, facilis. Quam, autem!";
+const TEXT_ABOUT_QUEST_TWO = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ut iusto adipisci id soluta eligendi, magni similique molestiae. Illo culpa mollitia dolore, neque cumque aliquid tenetur. Voluptates, facilis. Quam, autem!";
+const TEXT_ABOUT_QUEST_FREE = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ut iusto adipisci id soluta eligendi, magni similique molestiae. Illo culpa mollitia dolore, neque cumque aliquid tenetur. Voluptates, facilis. Quam, autem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ut iusto adipisci id soluta eligendi, magni similique molestiae. Illo culpa mollitia dolore, neque cumque aliquid tenetur. Voluptates, facilis. Quam, autem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ut iusto adipisci id soluta eligendi, magni similique molestiae. Illo culpa mollitia dolore, neque cumque aliquid tenetur. Voluptates, facilis. Quam, autem!";
+const TEXT_ABOUT_QUEST_FOO = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.";
+
+//Default bonus text carousel and card
+const SLIDER_BONUS_TEXT_ONE = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae tempora nemo culpa harum similique necessitatibus.Quis.";
+const SLIDER_BONUS_TEXT_TWO = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae tempora nemo culpa harum similique necessitatibus.Quis.";
+const SLIDER_BONUS_TEXT_FREE = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae tempora nemo culpa harum similique necessitatibus.Quis.";
+const SLIDER_BONUS_TEXT_FOO = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae tempora nemo culpa harum similique necessitatibus.Quis.";
+const SLIDER_BONUS_TEXT_FIVE = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae tempora nemo culpa harum similique necessitatibus.Quis.";
+const SLIDER_BONUS_TEXT_SIX = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae tempora nemo culpa harum similique necessitatibus.Quis.";
+
+//Default comment text
+const COMMENT_TEXT = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, iure, nulla! Quibusdam ut vel nihil, laudantium cupiditate nesciunt natus excepturi voluptatibus, fuga deserunt dignissimos! Ea nobis laudantium placeat iste, sed?";
+
+//Default sertification text
+const SERTIFICATION_TEXT = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo culpa mollitia dolore, neque cumque aliquid tenetur. Voluptates, facilis. Quam, autem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati earum quae sit harum neque fuga sed, eveniet iure aliquid ipsam quos ipsa, laborum tenetur possimus repellat animi. Officiis similique, neque.";
---
+++ server/public/javascripts/jacor.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+    });
+});
+
+$(document).ready(function(){
+    $("#reservation_scroll").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+});
